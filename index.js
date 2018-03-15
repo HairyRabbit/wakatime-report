1c535542b168697cf51b883aa4fa978876eda1c3
Rabbit <yfhj1990@hotmail.com>
2018-03-15 10:01:05 +0800

    update

---
+++ index.js
@@ -0,0 +1,36 @@
+const request = require('request')
+
+const domain = 'www.wakatime.com'
+
+
+function fetchLeaderBoard(url) {
+    request(url)
+}
+
+function fetchUserInfo(url) {
+    request(url)
+}
+
+function render() {
+
+}
+
+function renderToImage() {
+
+}
+
+function main(main) {
+    class LeaderBoard {
+        constructor(name, id) {
+            this.name = name
+            this.id = id
+        }
+    }
+
+    const leaderboardUrls = [
+
+    ]
+}
+
+
+main()

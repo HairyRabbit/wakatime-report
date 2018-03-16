6a3d55c6c13839e4967d5390cdd3bf6f8caf2b5c
Rabbit <yfhj1990@hotmail.com>
2018-03-16 23:07:15 +0800

    update

--- index.js
+++ index.js
@@ -5,49 +5,43 @@ const { JSDOM } = require('jsdom')
 const jquery = require('jquery')
 const svg2png = require('svg2png')
 
-const domain = 'www.wakatime.com'
-
-function fetchLeaderBoard(url) {
-    request(url)
+function readSession() {
+  return fs.readFileSync('./session', 'utf-8')
 }
 
-function fetchUserInfo(url) {
-    request(url)
-}
+function getLeaderBoard(cookie) {
+  const urls = [
+    'https://wakatime.com/leaders/sec/%25E5%2585%2594%2526friends',
+    'https://wakatime.com/leaders/sec/%25E5%2585%2594%2526moreFriends',
+    'https://wakatime.com/leaders/sec/%25E5%2585%2594%2526friendsPlus'
+  ]
 
-function render() {
-
-}
-
-function renderToImage() {
-
-}
-
-function main(main) {
-    class LeaderBoard {
-        constructor(name, id) {
-            this.name = name
-            this.id = id
+  return Promise.all(urls.map(url => {
+    return new Promise((resolve, reject) => {
+      request({
+        url,
+        headers: {
+          cookie
         }
-    }
+      }, (err, _, data) => {
+        if(err) {
+          reject(err)
+          return
+        }
 
-    const leaderboardUrls = [
-      'https://wakatime.com/leaders/sec/%25E5%2585%2594%2526friends',
-      'https://wakatime.com/leaders/sec/%25E5%2585%2594%2526moreFriends',
-      'https://wakatime.com/leaders/sec/%25E5%2585%2594%2526friendsPlus'
-    ]
+        resolve(data)
+      })
+    })
+  }))
+}
 
-  request({
-    url: 'https://wakatime.com/leaders/sec/%25E5%2585%2594%2526friends',
-    method: 'GET',
-    headers: {
-      cookie: 'remember_token=28d23ab3-7e45-4db7-ac49-2916f62c97e1|d903a58077e316ea3d914c61477e856d83b4859fd92b32db046db894ec14aab11df38ac65ec49fecf910719f73dc278f010aa5e171cd6a9588236405a3659683;'
-    }
-  }, (err, res, body) => {
-    const { window } = new JSDOM(body)
+function getUsersData(datas) {
+  return Promise.all(datas.map(data => {
+    const { window } = new JSDOM(data)
     const $ = jquery(window)
+    const $users = $('[class^="rank"]').not('.current-user')
 
-    const users = $('[class^="rank"]').not('.current-user').toArray().map(el => {
+    return $users.toArray().map(el => {
       const $el = $(el)
       const $user = $(el).find('td').eq(1)
       const name = $user.text().trim()
@@ -64,16 +58,35 @@ function main(main) {
         avg
       }
     })
+  }))
+}
 
+function sortByPercent(a, b) {
+  const { percent: pa } = a
+  const { percent: pb } = b
+  return pa - pb
+}
+
+function mapDataByName(prop) {
+  return prop.sort(sortByPercent).map(({ name }) => name)
+}
+
+function getUsersDetails(cookie) {
+  return users => {
     return Promise.all(users.map(user => {
       const { id } = user
       return new Promise((resolve, reject) => {
         request({
           url: `https://wakatime.com/api/v1/users/${id}/stats/last_7_days?timeout=15`,
           headers: {
-            cookie: 'remember_token=28d23ab3-7e45-4db7-ac49-2916f62c97e1|d903a58077e316ea3d914c61477e856d83b4859fd92b32db046db894ec14aab11df38ac65ec49fecf910719f73dc278f010aa5e171cd6a9588236405a3659683;'
+            cookie
           }
         }, (err, res, body) => {
+          if(err) {
+            reject(err)
+            return
+          }
+
           const {
             languages,
             editors,
@@ -82,60 +95,75 @@ function main(main) {
             total_seconds
           } = JSON.parse(body).data
 
-          user.langs = languages
-            .sort(({ percent: pa }, { percent: pb }) => pa - pb)
-            .map(({ name }) => name)
-          user.editors = editors
-            .sort(({ percent: pa }, { percent: pb }) => pa - pb)
-            .map(({ name }) => name)
-          user.oss = operating_systems
-            .sort(({ percent: pa }, { percent: pb }) => pa - pb)
-            .map(({ name }) => name)
+          user.langs = mapDataByName(languages)
+          user.editors = mapDataByName(editors)
+          user.oss = mapDataByName(operating_systems)
           resolve(user)
         })
       })
     }))
-      .then(users => {
-        return Promise.all(users.map(user => {
-          const { avatar } = user
-          return new Promise((resolve, reject) => {
-            request({
-              url: avatar,
-              encoding: 'base64'
-            }, (err, res, data) => {
-              user.avatar = data
-              resolve(user)
-            })
-          })
-        }))
-      })
-      .then(users => {
-      console.log(users)
-      const wrapper = fs.readFileSync('./wrapper.svg', 'utf-8')
-      const header = fs.readFileSync('./header.svg', 'utf-8')
-      const items = users.map(({ id, name, avatar, sum, avg }, idx) => {
-        return fs.readFileSync('./item.svg', 'utf-8')
-          .replace(/{{Id}}/g, id)
-          .replace('{{Avatar}}', avatar)
-          .replace('{{Name}}', name)
-          .replace('{{Sum}}', sum)
-          .replace('{{Avg}}', avg)
-          .replace('{{Rank}}', idx + 1)
-          .replace('{{Height}}', String(idx * 45))
-      })
-
-      const svg = wrapper
-        .replace('{{Top}}', header.replace('{{Avatar}}', users[0].avatar))
-        .replace('{{List}}', items.join('\n'))
-
-      console.log(svg)
-      return svg2png(svg)
-    })
-      .then(buffer => {
-        fs.writeFileSync(`output.png`, buffer, 'utf-8')
-      })
-  })
+  }
 }
 
+function downloadAvatars(users) {
+  return Promise.all(users.map(user => {
+    const { avatar } = user
+    return new Promise((resolve, reject) => {
+      request({
+        url: avatar,
+        encoding: 'base64'
+      }, (err, res, data) => {
+        if(err) {
+          reject(err)
+          return
+        }
+        user.avatar = data
+        resolve(user)
+      })
+    })
+  }))
+}
+
+function render(users) {
+  const wrapper = fs.readFileSync('./wrapper.svg', 'utf-8')
+  const header = fs.readFileSync('./header.svg', 'utf-8')
+  const items = users.map(({ id, name, avatar, sum, avg }, idx) => {
+    return fs.readFileSync('./item.svg', 'utf-8')
+      .replace(/{{Id}}/g, id)
+      .replace(/{{Avatar}}/g, avatar)
+      .replace(/{{Name}}/g, name)
+      .replace(/{{Sum}}/g, sum)
+      .replace(/{{Avg}}/g, avg)
+      .replace(/{{Rank}}/g, idx + 1)
+      .replace(/{{Height}}/g, String(idx * 45))
+  })
+
+  const headers = header.replace('{{Avatar}}', users[0].avatar)
+  const svg = wrapper
+        .replace(/{{Top}}/, headers)
+        .replace(/{{List}}/g, items.join('\n'))
+
+  return svg
+}
+
+function output(svg) {
+  return svg2png(svg)
+    .then(buffer => {
+      fs.writeFileSync(`output.png`, buffer, 'utf-8')
+    })
+}
+
+
+function main(main) {
+  const session = readSession()
+  Promise.resolve(session)
+    .then(getLeaderBoard)
+    .then(getUsersData)
+    .then(getUsersDetails)
+    .then(downloadAvatars)
+    .then(render)
+    .then(console.log)
+    // .then(output)
+}
 
 main()
